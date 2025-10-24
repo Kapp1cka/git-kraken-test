ada5c6e84d87fb9486948e478b62d41d04df770c
Kapp1cka <simon.soltes07@gmail.com>
2025-10-24 11:56:47 +0200

    gregory droydu

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Simon from "./pages/Simon/Simon";
 
 export default function AppRoutes() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/gregory" element={<Simon/>}/>
       </Routes>
     </HashRouter>
   );
---
+++ src/pages/Simon/Simon.tsx
@@ -0,0 +1,7 @@
+export default function Simon() {
+  return (
+    <>
+        <h1>George Droyd Diddy Tron Charlie Kirkborg</h1>
+    </>
+  )
+}
